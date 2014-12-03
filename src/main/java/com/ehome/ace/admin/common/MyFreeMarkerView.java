544a2fee90e39cd20f5c0f84ac76ad9d1f53ae36
haoxiaolei <xiaolei-0228@163.com>
2014-12-03 18:49:10 +0800

    Ace Admin

---
+++ src/main/java/com/ehome/ace/admin/common/MyFreeMarkerView.java
@@ -0,0 +1,11 @@
+package com.ehome.ace.admin.common;
+
+import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
+
+/**
+ * Created by haoxiaolei on 2014-12-03 18:48
+ */
+public class MyFreeMarkerView extends FreeMarkerView {
+
+
+}
